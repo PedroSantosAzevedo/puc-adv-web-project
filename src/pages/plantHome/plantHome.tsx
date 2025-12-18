825f61a730dd680dbb18d007f298369325dc6dae
Pedro Azevedo <pedroazevedo@192.168.0.18>
2025-12-17 21:01:30 -0300

    adiciona animacao

--- src/pages/plantHome/plantHome.tsx
+++ src/pages/plantHome/plantHome.tsx
@@ -2,18 +2,48 @@ import DefaultButton from "../../components/defaultButton/defaultButton";
 import MyHeader from "../../components/header/header";
 import "./plantHome.css";
 import { useNavigate } from "react-router-dom";
+import {useEffect, useState} from "react";
+import leafImage from "../../assets/leaf.png";
+import flowerImage from "../../assets/oldLeaf.png";
 
 export default function PlantHome() {
-const  navigate = useNavigate();
+  const navigate = useNavigate();
+  const [currentImage, setCurrentImage] = useState(0);
+  const [fade, setFade] = useState(true);
+  
+  const images = [leafImage, flowerImage];
 
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setFade(false);
+      
+      setTimeout(() => {
+        setCurrentImage(prev => (prev + 1) % images.length);
+        setFade(true);
+      }, 700);
+      
+    }, 2000); 
 
-    return (
-      <div>
-        <MyHeader title="Bem vindo" />
-        <div className = "homeTextContainer">
-          <h2 className = "homeMainText" >Bem vindos ao meu site de controle de plantas caseiro. No futuro esse site se conctará à placas esp8266 que verificarão os status de humidade e assim como irão monitorar as datas das ultimas regras</h2>
-          <DefaultButton text="Minhas Plantas" onClick={() => navigate("/plants")} />
-        </div>
+    return () => clearInterval(interval);
+  }, [images.length]);
+
+  return (
+    <div>
+      <MyHeader title="Bem vindo" />
+      <div className="contentContainer">
+        <img 
+          src={images[currentImage]} 
+          className={`home-img ${fade ? 'fade-in' : 'fade-out'}`}
+          alt="Plant animation" 
+        />
+        <h2 className="homeMainText">
+          Bem vindos ao meu site de controle de plantas caseiro. No futuro esse site se conectará à placas esp8266 que verificarão os status de humidade e irão monitorar as datas das ultimas regras
+        </h2>
+        <DefaultButton 
+          text="Minhas Plantas" 
+          onClick={() => navigate("/plants")} 
+        />
       </div>
-    );
+    </div>
+  );
 }
